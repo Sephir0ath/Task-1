ea9e7fb48b4af72349459a7780f6e0a732e14c4a
sshiro0 <francnunez2023@inf.udec.cl>
2024-04-20 18:31:22 -0400

    Bebida Completado

--- Tarea 1/src/main/java/Classes/Bebida.java
+++ Tarea 1/src/main/java/Classes/Bebida.java
@@ -1,4 +1,11 @@
 package Classes;
 
-public class Bebida extends Producto{
+public abstract class Bebida extends Producto{
+    public Bebida(int serie){
+        super(serie);
+    }
+    public int getSerie(){
+        return super.getSerie;
+    }
+    public abstract String comer();
 }
