b330468285d93e32d5c0a3d444c86cdf868f144d
Kingsephir <feliperaysz3@gmail.com>
2024-04-20 18:38:28 -0400

    clase Deposito añadida

--- Tarea 1/src/main/java/Classes/Deposito.java
+++ Tarea 1/src/main/java/Classes/Deposito.java
@@ -2,13 +2,28 @@ package Classes;
 import java.util.ArrayList;
 
 public class Deposito<T> {
-    ArrayList<T> d;
+    ArrayList<T> deposito;
 
     public Deposito(){
-        ArrayList<T> d = new ArrayList<>();
-
-
+        deposito = new ArrayList<>();
     }
 
+    public T get(){
+        if (deposito.isEmpty()){
+            return null;
+        }
+
+        T objetoQueSeRetorna = deposito.get(0);
+        deposito.remove(0);
+        return objetoQueSeRetorna;
+    }
+
+    public ArrayList<T> returnArrayList(){
+        return deposito;
+    }
+
+    public void add(T objeto){
+        deposito.add(objeto);
+    }
 
 }
