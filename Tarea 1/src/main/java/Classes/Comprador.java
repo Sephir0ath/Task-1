d4038836a4b35ee595074fe3d5f07987b47a606d
sshiro0 <francnunez2023@inf.udec.cl>
2024-04-20 18:56:55 -0400

    Añadí Comprador

--- Tarea 1/src/main/java/Classes/Comprador.java
+++ Tarea 1/src/main/java/Classes/Comprador.java
@@ -1,7 +1,22 @@
 package Classes;
 
 public class Comprador {
-    public Comprador(){
-        System.out.println();
+    private String sonido;
+    private int vuelto;
+    public Comprador(Moneda moneda, Productos cualProducto, Expendedor){
+        sonido = expendedor.comprarProducto(moneda, cualProducto);
+        while(true) {
+            Moneda monedaAObtener = expendedor.getVuelto();
+            if (monedaAObtener == null){
+                break;
+            }
+            vuelto += monedaAObtener.getValor();
+        }
+    }
+    public int cuantoVuelto() {
+        return vuelto;
+    }
+    public String queComiste() {
+        return sonido;
     }
 }
