6cdaac15f40ffecc123ce22ded422192a3bf7f89
sshiro0 <francnunez2023@inf.udec.cl>
2024-04-20 18:43:50 -0400

    Añadí las bebidas

---
+++ Tarea 1/src/main/java/Classes/CocaCola.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class CocaCola extends Bebida {
+    public CocaCola(int serie){
+        super(serie);
+    }
+    @Override
+    public String comer(){
+        return "CocaCola";
+    }
+}
