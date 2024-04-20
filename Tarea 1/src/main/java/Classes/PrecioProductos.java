aacad4e9a33ad950ef78b37d9e1701989d26dd56
Kingsephir <feliperaysz3@gmail.com>
2024-04-20 18:32:23 -0400

    añadido los enum

--- Tarea 1/src/main/java/Classes/PrecioProductos.java
+++ Tarea 1/src/main/java/Classes/PrecioProductos.java
@@ -1,4 +1,20 @@
 package Classes;
 
 public enum PrecioProductos {
+    VALORCOCACOLA(1000),
+    VALORSPRITE(800),
+    VALORFANTA(700),
+    VALORSNICKERS(600),
+    VALORSUPER8(200);
+
+    private final int precio;
+
+    private PrecioProductos(int precio){
+        this.precio = precio;
+    }
+
+    public int getPrecio(){
+       return precio;
+    }
+
 }
--- Tarea 1/src/main/java/Classes/Productos.java
+++ Tarea 1/src/main/java/Classes/Productos.java
@@ -1,5 +1,9 @@
 package Classes;
 
 public enum Productos {
-
+    COCA,
+    SPRITE,
+    FANTA,
+    SUPER8,
+    SNICKERS;
 }
