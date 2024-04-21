2fb199694612699d423c9bed0e735312b9c34a15
sshiro0 <francnunez2023@inf.udec.cl>
2024-04-21 12:19:37 -0400

    Añadido el Javadoc de Dulce

--- Tarea 1/src/main/java/Classes/Dulce.java
+++ Tarea 1/src/main/java/Classes/Dulce.java
@@ -1,28 +1,29 @@
 package Classes;
 
 /**
- *
+ * Clase abstracta que representa los dulces, es subclase de Producto
+ * Esta clase es extendida por Super8 y Snickers
  */
 public abstract class Dulce extends Producto {
     /**
-     *
-     * @param serie
+     * Constructor por defecto
+     * @param serie Recibe el número de serie del dulce
      */
     public Dulce(int serie) {
         super(serie);
     }
 
     /**
-     *
-     * @return
+     * Método para obtener la serie del dulce
+     * @return int del número de serie de el producto
      */
     public int getSerie() {
         return super.getSerie();
     }
 
     /**
-     *
-     * @return
+     * Método abstracto que representa que el producto fue consumido
+     * @return String que contiene el nombre del producto consumido
      */
     public abstract String comer();
 }
