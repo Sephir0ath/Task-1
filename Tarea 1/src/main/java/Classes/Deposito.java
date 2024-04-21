b6a4558db2468045be6c4779eed124019154ac72
Kingsephir <feliperaysz3@gmail.com>
2024-04-21 12:15:55 -0400

    Añadido Javadoc para Deposito

--- Tarea 1/src/main/java/Classes/Deposito.java
+++ Tarea 1/src/main/java/Classes/Deposito.java
@@ -2,22 +2,24 @@ package Classes;
 import java.util.ArrayList;
 
 /**
+ * Se utiliza para simular un deposito en donde se pueden almacenar y sacar productos
  *
- * @param <T>
+ * @param <T> Parametro de tipo generico, define el tipo de deposito
  */
 public class Deposito<T> {
     ArrayList<T> deposito;
 
     /**
-     *
+     * Constructor de la clase, inicializa la variable deposito
      */
     public Deposito(){
         deposito = new ArrayList<>();
     }
 
     /**
+     * metodo para obtener un elemento del ArrayList deposito
      *
-     * @return
+     * @return un objeto generico, en caso de que el deposito esté vacío retorna vacío (null)
      */
     public T get(){
         if (deposito.isEmpty()){
@@ -30,16 +32,18 @@ public class Deposito<T> {
     }
 
     /**
+     * metodo para obtener el ArrayList deposito dentro de la clase Comprador
      *
-     * @return
+     * @return objeto tipo Arraylist
      */
     public ArrayList<T> returnArrayList(){
         return deposito;
     }
 
     /**
+     * metodo para añadir un objeto tipo generico a un deposito
      *
-     * @param objeto
+     * @param objeto Objeto tipo generico que representa un producto o una moneda
      */
     public void add(T objeto){
         deposito.add(objeto);
