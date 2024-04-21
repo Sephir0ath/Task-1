cc9e04827d3d75162411bb735b95f6bf38f2373c
sshiro0 <francnunez2023@inf.udec.cl>
2024-04-21 11:47:23 -0400

    Añadido el Javadoc para Moneda

--- Tarea 1/src/main/java/Classes/Moneda.java
+++ Tarea 1/src/main/java/Classes/Moneda.java
@@ -1,24 +1,29 @@
 package Classes;
 
 /**
- *
- *
+ * Esta clase representa las monedas que son ingresadas al expendedor con las cuales se compran los productos
+ * Moneda es extendida por Moneda100, Moneda500, Moneda1000 y Moneda 1500
  */
 public abstract class Moneda implements Comparable<Moneda>{
     /**
-     *
-     *
+     * Constructor por defecto
      */
-    public Moneda(){}
+    public Moneda(){
 
+    }
+
+    /**
+     * Método que retorna la serie de la moneda
+     * @return serie de la moneda
+     */
     public Moneda getSerie(){
         return this;
     }
 
     /**
-     *
-     * @param moneda the object to be compared.
-     * @return
+     * Método para comparar el valor entre monedas
+     * @param moneda moneda a comparar
+     * @return 1 si las monedas tienen igual valor, 0 si son distintos sus valores
      */
     @Override
     public int compareTo(Moneda moneda) {
@@ -32,8 +37,8 @@ public abstract class Moneda implements Comparable<Moneda>{
     }
 
     /**
-     *
-     * @return
+     * Retorna el valor numérico de la moneda 100, 500, 1000 o 1500 dependiendo de la moneda
+     * @return int del valor de la moneda
      */
     public abstract int getValor();
 
