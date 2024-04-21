411f60eb68668213c6ae450620ac32ab181d187d
Mazulin <matijenven2004@gmail.com>
2024-04-21 12:06:28 -0400

    Añadido el Javadoc para Fanta

--- Tarea 1/src/main/java/Classes/Fanta.java
+++ Tarea 1/src/main/java/Classes/Fanta.java
@@ -2,12 +2,12 @@ package Classes;
 import Classes.Bebida;
 
 /**
- *
+ * Fanta Subclase de Bebida
  */
 public class Fanta extends Bebida {
     /**
      *
-     * @param serie
+     * @param serie Recibe el numero de serie del producto
      */
     public Fanta(int serie){
         super(serie);
@@ -15,7 +15,7 @@ public class Fanta extends Bebida {
 
     /**
      *
-     * @return
+     * @return Retorna "Fanta" al ser tomada, correspondiente a la marca de la Bebida
      */
     @Override
     public String comer(){
