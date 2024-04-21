08c1bb973d39daea4b66be18a44ee82788d60b33
Mazulin <matijenven2004@gmail.com>
2024-04-21 12:17:09 -0400

    Añadido el Javadoc para Producto

--- Tarea 1/src/main/java/Classes/Producto.java
+++ Tarea 1/src/main/java/Classes/Producto.java
@@ -1,14 +1,14 @@
 package Classes;
 
 /**
- *
+ * Producto Clase de tipo abstracta, sirve de base para Productos como Dulces y Bebidas
  */
 public abstract class Producto {
     private int serie;
 
     /**
      *
-     * @param serie
+     * @param serie Recibe el numero de serie del producto
      */
     public Producto(int serie){
         this.serie = serie;
@@ -16,7 +16,7 @@ public abstract class Producto {
 
     /**
      *
-     * @return
+     * @return serie Retorna el numero de serie del producto
      */
     public int getSerie(){
         return serie;
@@ -24,7 +24,7 @@ public abstract class Producto {
 
     /**
      *
-     * @return
+     * @return Retorna un String con el nombre del producto al ser usado
      */
     public abstract String comer();
 }
