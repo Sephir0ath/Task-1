31bb05a48d4744b0f5fbf3a1047eda8f3a100b35
Mazulin <matijenven2004@gmail.com>
2024-04-21 11:50:22 -0400

    Añadido el Javadoc para Bebida

--- Tarea 1/src/main/java/Classes/Bebida.java
+++ Tarea 1/src/main/java/Classes/Bebida.java
@@ -1,12 +1,12 @@
 package Classes;
 
 /**
- *
+ *Bebida Subclase de Producto de tipo abstracta, sirve de base para las diferentes marcas de Bebidas
  */
 public abstract class Bebida extends Producto{
     /**
      *
-     * @param serie
+     * @param serie Recibe el numero de serie del producto
      */
     public Bebida(int serie){
         super(serie);
@@ -14,7 +14,7 @@ public abstract class Bebida extends Producto{
 
     /**
      *
-     * @return
+     * @return Retorna el numero de serie del producto
      */
     public int getSerie(){
         return super.getSerie();
@@ -22,7 +22,7 @@ public abstract class Bebida extends Producto{
 
     /**
      *
-     * @return
+     * @return Retorna un String con el nombre del producto al ser usado
      */
     public abstract String comer();
 }
